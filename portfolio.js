a99e40cd4ffd6ceb644f8f0f620c93d1b7841ed9
CofiShahcou <filipsavic230@gmail.com>
2023-11-26 00:09:58 +0100

    latice

--- portfolio.js
+++ portfolio.js
@@ -1,23 +1,23 @@
 // ⁡⁢⁣⁣𝗢𝘃𝗼 𝗷𝗲 𝘀𝗮𝗺𝗼 𝗴𝗼𝗱 𝗿𝗮𝗱𝗶𝗺 𝗱𝗮 𝗺𝗶 𝗻𝗲 𝘀𝗺𝗲𝘁𝗮𝗷𝘂 𝗮𝗻𝗶𝗺𝗮𝗰𝗶𝗷𝗲⁡
-$("body").css("background-color", "var(--boja3)");
-$(".onoKaoBasSve").css("opacity", "1");
-$(".latice").css("display", "none");
+// $("body").css("background-color", "var(--boja3)");
+// $(".onoKaoBasSve").css("opacity", "1");
+// $(".latice").css("display", "none");
 
 // ⁡⁢⁣⁣!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!⁡
 // ⁡⁢⁣⁣𝗢𝘃𝗼 𝗷𝗲 𝗸𝗼𝗱 𝗸𝗼𝗷𝗶 𝘁𝗿𝗲𝗯𝗮 𝗱𝗮 𝘂𝗸𝗹𝗷𝘂𝗰𝗶𝗺 𝗸𝗮𝗱𝗮 𝘇𝗮 𝘃𝗿𝘀𝗶𝗺 𝘀𝗮𝗷𝘁⁡
-// setTimeout(() => {
-//     function load() {
-//         $(".latice").css("opacity", "0");
-//         setTimeout(() => {
-//             $(".onoKaoBasSve").css("opacity", "1");
-//             $("body").css("background-color", "var(--boja3)");
-//         }, 500);
-//     }
-//     document.onload = load();
-//     setTimeout(() => {
-//         $(".latice").css("display", "none");
-//     }, 2000);
-// }, 10000);
+setTimeout(() => {
+    function load() {
+        $(".latice").css("opacity", "0");
+        setTimeout(() => {
+            $(".onoKaoBasSve").css("opacity", "1");
+            $("body").css("background-color", "var(--boja3)");
+        }, 500);
+    }
+    document.onload = load();
+    setTimeout(() => {
+        $(".latice").css("display", "none");
+    }, 2000);
+}, 10000);
 // ⁡⁢⁣⁣𝗢𝘃𝗼 𝗷𝗲 𝗸𝗼𝗱 𝗸𝗼𝗷𝗶 𝘁𝗿𝗲𝗯𝗮 𝗱𝗮 𝘂𝗸𝗹𝗷𝘂𝗰𝗶𝗺 𝗸𝗮𝗱𝗮 𝘇𝗮 𝘃𝗿𝘀𝗶𝗺 𝘀𝗮𝗷𝘁
 // ⁡⁢⁣⁣!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
